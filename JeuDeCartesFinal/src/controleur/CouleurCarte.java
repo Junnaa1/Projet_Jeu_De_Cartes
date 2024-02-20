46a6b7b015de2cd1126d5914aff0dae0d20079ed
Quentinho <quentin.ayral@hotmail.com>
2024-02-20 13:28:00 +0100

    ajout CouleurCarte

---
+++ JeuDeCartesFinal/src/controleur/CouleurCarte.java
@@ -0,0 +1,28 @@
+package controleur;
+
+public enum CouleurCarte {
+    TREFLE(1),
+    PIQUE(2),
+    CARREAU(3),
+    COEUR(4);
+
+    private final int points;
+
+    CouleurCarte(int points) {
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public static CouleurCarte getCouleurCarteFromString(String unNomDeCouleur) {
+        for (CouleurCarte couleurCarte : CouleurCarte.values()) {
+            if (couleurCarte.name().equals(unNomDeCouleur)) {
+                return couleurCarte;
+            }
+        }
+        return null;
+    }
+}
+
