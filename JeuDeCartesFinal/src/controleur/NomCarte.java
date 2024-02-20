5cd044b6c20e0977011fc6af63e49953c50d7813
Quentinho <quentin.ayral@hotmail.com>
2024-02-20 13:13:02 +0100

    ajout NomCarte

---
+++ JeuDeCartesFinal/src/controleur/NomCarte.java
@@ -0,0 +1,27 @@
+package controleur;
+
+public enum NomCarte {
+    DEUX(2),
+    TROIS(3),
+    QUATRE(4),
+    CINQ(5),
+    SIX(6),
+    SEPT(7),
+    HUIT(8),
+    NEUF(9),
+    DIX(10),
+    VALET(11),
+    DAME(12),
+    ROI(13),
+    AS(14);
+
+    private final int points;
+
+    NomCarte(int points) {
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
