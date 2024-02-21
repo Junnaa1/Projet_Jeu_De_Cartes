403b46e8159e35f9f6a1e828bdbed2cd70af43eb
Junnaa1 <aymerickrakoto@gmail.com>
2024-02-21 17:47:41 +0100

    ajout bibliotheques pour l'interface utilisateur

--- JeuDeCartesFinal/src/vue/Gui.java
+++ JeuDeCartesFinal/src/vue/Gui.java
@@ -1,5 +1,16 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 public class Gui {
 
 }
